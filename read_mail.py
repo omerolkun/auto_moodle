4020e8fff7dbb89ff79c3051eb57bf31d1c83bd8
omerolkun <omer.olkun@ug.bilkent.edu.tr>
2021-11-16 14:38:04 +0300

    berdans code

--- read_mail.py
+++ read_mail.py
@@ -1,23 +1,46 @@
 import imaplib
+import email
+from email.header import decode_header
+import webbrowser
+import os
 
-mail = imaplib.IMAP4_SSL('mail.bilkent.edu.tr')
-mail.login('omer.olkun@ug.bilkent.edu.tr', 'b101557')
-mail.list()
-# Out: list of "folders" aka labels in gmail.
-mail.select("inbox") # connect to inbox.
+def read_mails(u,p):
+    # account credentials
+    # create an IMAP4 class with SSL
+    imap = imaplib.IMAP4_SSL("mail.bilkent.edu.tr")
+    # authenticate
+    imap.login(u, p)
 
-result, data = mail.search(None, 'ALL' )
+    status, messages = imap.select("INBOX")
 
-ids = data[0] # data is a list.
-id_list = ids.split() # ids is a space separated string
-latest_email_id = id_list[-1] # get the latest
 
-result, data = mail.fetch(latest_email_id, "(RFC822)") # fetch the email body (RFC822)             for the given ID
+    mail_numbers = int(messages[0])
+    #mailler = imap.search("starsmsg@bilkent.edu.tr")
+    #print(mail_numbers)
 
-raw_email = data[0][1] # here's the body, which is raw text of the whole email
-# including headers and alternate payloads
-x = type(raw_email)
-a = raw_email.decode("utf-8") 
-print(a[(len(a)-7):])
+    subject_en = 'SUBJECT "Secure Login Gateway E-Mail Verification Code"'
+    subject_tr = 'SUBJECT "Güvenli Giriş Kapısı E-Posta Doğrulama Kodu"'
+    #subject_tr = 'SUBJECT "Secure Login Gateway E-Mail Verification Code"'
 
-print(a)
+
+    search_s = '(OR (%s) (%s))'%(subject_en, subject_tr)
+
+    #print(search_s)
+    data = imap.search('utf-8', search_s.encode("utf-8"))[1][0].decode("utf-8").split()
+    #print(data)
+    res, msg = imap.fetch(str(data[-1]), "(RFC822)")
+
+    mes = msg[0][1].decode("utf-8")
+
+    words = email.message_from_string(mes).get_payload().split()
+    #print(words)
+
+    if(words[1] == "kodlu"):
+        return  (words[0],words[-1])
+    elif words[0] == "Verification":
+        return (words[-1][:-1], words[2])
+
+    return email.message_from_string(mes).get_payload().split()[2]
+
+
+print(read_mails("omer.olkun@ug.bilkent.edu.tr","b101557"))
