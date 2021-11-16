b8421f1248116bd229f7c0fe8f37431355e12b48
Berdan Akyürek <berdan.akyurek@ug.bilkent.edu.tr>
2021-11-16 15:55:44 +0300

    Personal configuration

--- sele.py
+++ sele.py
@@ -3,10 +3,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from time import sleep
-from credits import id, moddle_p, mail_adr, mail_p
-driver = webdriver.Chrome('/home/omergenc/TEST_BOARD/auto_moodle/chromedriver')
+from credits import id, moodle_p, mail_adr, mail_p
+driver = webdriver.Chrome()
 driver.get("https://stars.bilkent.edu.tr/srs")
-print(driver.title)
+#print(driver.title)
 
 sleep(1)
 
@@ -16,7 +16,7 @@ username = driver.find_element(By.ID, 'LoginForm_username')
 username.send_keys(id)
 
 p = driver.find_elements_by_tag_name("input")
-p[1].send_keys(moddle_p)
+p[1].send_keys(moodle_p)
 #p_word = driver.find_ele#ment(By.ID, 'LoginForm-p6fc250ddad')
 
 
@@ -29,7 +29,7 @@ confirmation_code.send_keys(read_mails(mail_adr,mail_p)[1])
 
 driver.find_element_by_name("yt0").click()# confirmation code submit
 
-
+print("Done")
 
 
 
@@ -37,4 +37,3 @@ driver.find_element_by_name("yt0").click()# confirmation code submit
 
 
 #driver.close()
-
