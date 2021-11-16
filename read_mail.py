1bfbc0eeefc5f603e8baf0e7fe75636e932da287
omerolkun <omer.olkun@ug.bilkent.edu.tr>
2021-11-16 14:36:06 +0300

    with substring

--- read_mail.py
+++ read_mail.py
@@ -16,5 +16,8 @@ result, data = mail.fetch(latest_email_id, "(RFC822)") # fetch the email body (R
 
 raw_email = data[0][1] # here's the body, which is raw text of the whole email
 # including headers and alternate payloads
+x = type(raw_email)
+a = raw_email.decode("utf-8") 
+print(a[(len(a)-7):])
 
-print(raw_email)
+print(a)
